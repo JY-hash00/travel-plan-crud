b6de66cb0d4360c6943bd1fcc302b014f7e2b783
JY-hash00 <jjangye0819@gmail.com>
2026-06-07 18:34:42 +0900

    Spring Boot 프로젝트 생성(Gradle, JPA, H2, Mustache)

---
+++ src/main/java/com/example/travel_plan_crud/TravelPlanCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_plan_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlanCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel_plan_crud/TravelPlanCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_plan_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlanCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
